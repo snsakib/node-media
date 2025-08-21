926a6871d421156fea0368889a55f17b652999fb
snsakib <hello@snsakib.com>
2025-08-21 21:43:58 +0600

    feat: implement file upload functionality with multer and update frontend form

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,48 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-app.post('/', (req, res) => {
-  console.log(req.body); // Logs the parsed JSON body
-  res.json({ status: 'ok' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+const upload = multer({ storage });
+
+app.post(
+  "/",
+  upload.fields([
+    { name: "single_image", maxCount: 1 },
+    { name: "multiple_images", maxCount: 2 }, // adjust maxCount as needed
+    { name: "single_video", maxCount: 1 },
+    { name: "multiple_videos", maxCount: 2 },
+    { name: "single_audio", maxCount: 1},
+    { name: "multiple_audios", maxCount: 2}
+  ]),
+  (req, res) => {
+    console.log("Body:", req.body); // Other form fields
+    console.log("Single File:", req.files["single_image"]); // Array with 1 file or undefined
+    console.log("Multiple Files:", req.files["multiple_images"]); // Array of files or undefined
+    console.log("Single Video:", req.files["single_video"]);
+    console.log("Multiple Videos:", req.files["multiple_videos"]); // Array of files or undefined
+    console.log("Single Audio:", req.files["single_audio"]);
+    console.log("Multiple Audios:", req.files["multiple_audios"]);
+    res.json({ status: "ok" });
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
 app.listen(port, () => {
--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
+  const maxFiles = 2;
+  $("#multiple_images").on("change", function () {
+    if (this.files.length > maxFiles) {
+      alert(`You can only select up to ${maxFiles} images.`);
+      this.value = "";
+    }
+  });
+
   $("#my_form").on("submit", function (e) {
     e.preventDefault();
-    console.log("Form submitted");
-    const payload = {
-      title: $("#title").val()
-    }
+
+    const formData = new FormData(this);
+
     $.ajax({
       url: "http://localhost:3000/",
       method: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(payload),
+      contentType: false,
+      processData: false,
+      data: formData,
       success: function (response) {
         console.log(response);
       },
